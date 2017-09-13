05cb9cdd532571220b639ecdc57ea7722ec44838
rounakpolley <rounakpolley19972014@gmail.com>
2017-09-13 16:25:41 +0530

    Uploading changes

--- README.md
+++ README.md
@@ -3,21 +3,43 @@ One Page Layout Of a startup page
 
 use correct html secmantics
 
-Increase the height of nav...
+use modal like disabled backgrounnd for nav toggle
+
+*** nav toggle slide from left
+
+fix nav drop down in desktop - color , mobile - text alignment
+Need
+1. download images
+2. google fonts api
+
 Mandetory
-1. OPL fixed nav - affix
-2. Hero area animation
-3. Subtle Parallax
-4. Flat styles - attractive
-5. Auto scroll down button on hero area
-//contact - flat modal like the alumni
-//smooth scroll
-//parrallax
+1. OPL fixed nav - affix  - DONE
+2. Hero area animation 
+3. Subtle Parallax  - DONE
+4. Flat styles - attractive ****** MOST IMP ********
+5. Auto scroll down button on hero area - DONE
+
+6. smooth scroll - DONE using js
+7. contact - flat modal like the alumni
+
+8. Use the panel loop through from codepen
 //hero text... (description)
-//use temlate.... verticle divide fll filled up rows...
-// back to top icon --- floting right corner...
+
+???//use template.... verticle divide fll filled up rows...
+// back to top icon --- floting right corner... --- no need as fixed navbar
 // cta on hero area
+
+issues
+1. nav Increase the height of nav...
+2. scroll spy
+3.
 
 Try
 1. Collapsed nav comes out from the left
-2. Custom scroll bar
+2. Custom scroll bar like slack
+
+
+//$('.navbar-toggle').toggleClass('indexcity');
+.indexcity {
+  z-index: 9999;
+}
--- scripts/oplscripts.js
+++ scripts/oplscripts.js
@@ -1,13 +1,24 @@
 $(document).ready(function()
-{     
+{   
     var scroll_start = 0;               //starting point of scroll
     var startchange = $('#explore');    //id of 'target element' from where nav style changes
     var offset = startchange.offset();  //offset coordinates for 'target element', relative to the document
     $('.navbar-inverse').css('background-color', 'rgba(0,0,0,0)');
-  
-    $(document).scroll(function() 
+    
+    $('.navbar-toggle').click(function()
+    {  
+        $('.navbar-collapse').toggleClass('nav-mobile-slide');
+        $('.navbar-collapse.navbar-right.nav-mobile-slide.collapse.in').css('margin-top', '-53px');
+    });
+    
+    $(document).scroll(function() // navbar style not changing.
     { 
-            scroll_start = $(this).scrollTop(); //current vertical position of the scroll bar 
+            scroll_start = $(this).scrollTop(); //current vertical position of the scroll bar
+
+            if(scroll_start < 53)
+            {   $('.navbar-collapse.navbar-right.nav-mobile-slide.collapse.in').css('margin-top', -scroll_start); }
+            else
+            {   $('.navbar-collapse.navbar-right.nav-mobile-slide.collapse.in').css('margin-top', '0px'); }
             if(scroll_start > offset.top)
             {   
                 $('.navbar-inverse').css('background-color', 'rgba(0,0,0,0)');
@@ -17,34 +28,21 @@ $(document).ready(function()
                 $('.navbar-inverse').css('background-color', '#222222');
             }
      });
+    $('.navbar-brand').click(function(evt)
+    {   evt.preventDefault();   $('html, body').animate({ scrollTop: $('#banner-area').offset().top }, 1000); });
     $('#nav-about').click(function(evt)
-    {
-        evt.preventDefault();
-        $('html, body').animate({ scrollTop: $('#startup_about').offset().top - 53 }, 1000);
-    });
+    {   evt.preventDefault();   $('html, body').animate({ scrollTop: $('#startup_about').offset().top }, 1000);    });
     $('#nav-projects-0').click(function(evt)
-    {
-        evt.preventDefault();
-        $('html, body').animate({ scrollTop: $('#startup_projects_0').offset().top - 53 }, 1000);
-    });
+    {   evt.preventDefault();   $('html, body').animate({ scrollTop: $('#startup_projects_0').offset().top}, 1000); });
     $('#nav-projects-1').click(function(evt)
-    {
-        evt.preventDefault();
-        $('html, body').animate({ scrollTop: $('#startup_projects_1').offset().top - 53 }, 1000);
-    });
+    {   evt.preventDefault();   $('html, body').animate({ scrollTop: $('#startup_projects_1').offset().top}, 1000); });
     $('#nav-team').click(function(evt)
-    {
-        evt.preventDefault();
-        $('html, body').animate({ scrollTop: $('#startup_team').offset().top - 53 }, 1000);
-    });
+    {   evt.preventDefault();   $('html, body').animate({ scrollTop: $('#startup_team').offset().top }, 1000); });
     $('#nav-blog').click(function(evt)
-    {
-        evt.preventDefault();
-        $('html, body').animate({ scrollTop: $('#startup_blogs').offset().top - 53 }, 1000);
-    });
+    {   evt.preventDefault();   $('html, body').animate({ scrollTop: $('#startup_blogs').offset().top }, 1000);    });
     $('#explore').click(function(evt)
     {
         evt.preventDefault();
-        $('html, body').animate({ scrollTop: $('#startup_about').offset().top - 53 }, 1000);
+        $('html, body').animate({ scrollTop: $('#startup_about').offset().top }, 1000);
     });
 });
